8fdee5ecc67aed977bd88d947ae3bd4f8befde50
MertTheGreat <merty.ertugrul@gmail.com>
2019-12-18 19:02:10 +0000

    Initial Commit

---
+++ Promodo Timer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Promodo Timer
+//
+//  Created by Mert's Macbook on 18.12.2019.
+//  Copyright © 2019 Mert's Macbook. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
